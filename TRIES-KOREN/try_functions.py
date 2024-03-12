55fb5b477da2453591a9d072d5cf90840fedb7ca
koren-yeruham <koren381@gmail.com>
2024-03-12 16:41:04 +0200

    pushing new file

--- TRIES-KOREN/try_functions.py
+++ TRIES-KOREN/try_functions.py
@@ -1,6 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 ###test-1.1.1
 def test_edit_first_name():
-
     my_account_btn = my.driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")
     my_account_btn.click()
     personal_details = my.driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
@@ -13,3 +20,4 @@ def test_edit_first_name():
     assert first_name_inner == 'נועה'
     save_btn = my.driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
     save_btn.click()
+
