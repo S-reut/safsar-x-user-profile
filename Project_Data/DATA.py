01c7ee97269bc6b3d015e38e96e431ecbc6a4dd3
koren-yeruham <koren381@gmail.com>
2024-03-13 19:17:08 +0200

    pushing new all

---
+++ Project_Data/DATA.py
@@ -0,0 +1,36 @@
+base_url = "https://portal-dev.safsarglobal.link/"
+###test-1.1.1
+#####edit_first_name:
+    first_name_1 = 'נועה'
+    my_account_btn = my.driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")
+    personal_details = my.driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
+    edit_profile = my.driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
+    first_name = my.driver.find_element(By.CSS_SELECTOR, "ant-input css-3mqfnx border border-primaryDark focus:outline-none focus:border-primaryPurple p-1 rounded-full")
+    save_btn = my.driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
+
+
+###test-1.1.2
+####edit_last_name:
+    my_account_btn = my.driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")
+    personal_details = my.driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
+    edit_profile = my.driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
+    last_name = my.driver.find_element(By.CSS_SELECTOR, "ant-input css-3mqfnx border border-primaryDark focus:outline-none focus:border-primaryPurple p-1 rounded-full mr-4")
+    last_name_inner = last_name.get_attribute('innerText')
+    save_btn = my.driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
+
+
+###test-1.1.3
+##edit_email_113:
+    my_account_btn = my.driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")
+    personal_details = my.driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
+    edit_profile = my.driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
+    email_field = my.driver.find_element(By.CSS_SELECTOR, "ant-input css-3mqfnx w-full border border-primaryDark focus:outline-none focus:border-primaryPurple p-1 rounded-full"
+                                         or By.ID, "email")
+    save_btn = my.driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
+
+
+
+###test-1.1.14
+##click_edit_tab:
+    my_account_btn = my.driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")    personal_details = my.driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
+    edit_profile = my.driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
---
+++ TRIES-KOREN/function_open_driver.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def driver_set():
+    chrome_options = Options()
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_argument('disable-extensions')
+
+    chrome_service = Service(ChromeDriverManager().install())
+
+    my_driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return my_driver
