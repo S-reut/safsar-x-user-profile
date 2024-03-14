72ebbb04ee0164a59ca610f01203cf87906f7ecb
koren-yeruham <koren381@gmail.com>
2024-03-14 15:34:01 +0200

    NEW

--- Project_Data/DATA.py
+++ Project_Data/DATA.py
@@ -5,9 +5,9 @@ login_btn_xpath = "//button[contains(text(),'כניסה')]"
     first_name_1 = 'נועה'
     my_account_btn = my_driver.find_element(By.CSS_SELECTOR, "sm:flex border border-white rounded-full hidden text-primaryLight hover:text-primaryPurple font-bold px-8 py-2")
     personal_details = my_driver.find_element(By.CSS_SELECTOR, "ant-tabs-tab-btn")
-    edit_profile =  my_driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
-    first_name =  my_driver.find_element(By.CSS_SELECTOR, "ant-input css-3mqfnx border border-primaryDark focus:outline-none focus:border-primaryPurple p-1 rounded-full")
-    save_btn =  my_driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
+    edit_profile = my_driver.find_element(By.CSS_SELECTOR, "font-bold text-primaryPurple px-4 py-2 underline underline-offset-2")
+    first_name = my_driver.find_element(By.CSS_SELECTOR, "ant-input css-3mqfnx border border-primaryDark focus:outline-none focus:border-primaryPurple p-1 rounded-full")
+    save_btn = my_driver.find_element(By.CSS_SELECTOR, "ant-btn css-3mqfnx ant-btn-primary w-[130px] text-sm bg-primaryPurple text-white font-bold pb-4 rounded-full my-2 hover:bg-primaryPurple")
 
 
 ###test-1.1.2
