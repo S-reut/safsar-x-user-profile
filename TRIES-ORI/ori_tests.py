4cd177bd15fe010dff175011da91ebf6645dcd2e
Ori Baider <oribaider@gmail.com>
2024-03-16 17:13:00 +0200

    logging into page

--- TRIES-ORI/ori_tests.py
+++ TRIES-ORI/ori_tests.py
@@ -6,9 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
-
-
+from selenium.webdriver.common.keys import Keys
 
 "rc-tabs-0-tab-כרטיסים"
 
